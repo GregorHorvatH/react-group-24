65a904093db06aaf90da6eb0147f6b285fd8d557
Geri <gregor.horvath83@gmail.com>
2021-02-24 13:44:16 +0200

    update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,24 @@
 import moment from 'moment';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import Divider from '@material-ui/core/Divider';
+import { green } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
   todoItem: {
     backgroundColor: '#ffeeff',
     marginTop: 10,
     position: 'relative',
+  },
+  cardContent: {
+    display: 'flex',
+    alignItems: 'center',
   },
   cardActions: {
     display: 'flex',
@@ -31,30 +39,43 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
 
   const handleToggle = () => onToggle(id);
   const handleDelete = () => onDelete(id);
+  const color =
+    urgency === 'high'
+      ? 'secondary'
+      : urgency === 'medium'
+      ? 'action'
+      : 'primary';
 
   return (
     <Card className={[classes.todoItem, classes[urgency]].join(' ')}>
-      <CardContent>
-        <p>{value}</p>
-        <p>urgency: {urgency}</p>
-      </CardContent>
-      <CardActions className={classes.cardActions}>
-        <span>{moment(date).format('YYYY/MM/DD hh:mm:ss')}</span>
+      <CardActionArea onClick={handleToggle}>
+        <CardContent className={classes.cardContent}>
+          <BookmarkIcon
+            color={color}
+            style={isDone ? { color: green[500] } : null}
+          />
+          <span>{value}</span>
+        </CardContent>
 
-        <label>
-          <input type="checkbox" onChange={handleToggle} value={isDone} />
-          <span>mark as done</span>
-        </label>
+        <Divider />
+        <CardActions className={classes.cardActions}>
+          <span>{moment(date).format('YYYY/MM/DD hh:mm:ss')}</span>
 
-        <IconButton
-          color="primary"
-          aria-label="delete todo"
-          component="span"
-          onClick={handleDelete}
-        >
-          <DeleteOutlineIcon />
-        </IconButton>
-      </CardActions>
+          <label>
+            <input type="checkbox" onChange={handleToggle} checked={isDone} />
+            <span>mark as done</span>
+          </label>
+
+          <IconButton
+            color="primary"
+            aria-label="delete todo"
+            component="span"
+            onClick={handleDelete}
+          >
+            <DeleteOutlineIcon fontSize="small" />
+          </IconButton>
+        </CardActions>
+      </CardActionArea>
     </Card>
   );
 };
